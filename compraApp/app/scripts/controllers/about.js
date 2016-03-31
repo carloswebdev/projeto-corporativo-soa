141d2aa3867f3e0eaa7619823b1a78fd817b4214
Felipe Moraes <felipealvesdemoraes@gmail.com>
2016-03-31 01:43:03 -0300

    V1
    Signed-off-by: Felipe Moraes <felipealvesdemoraes@gmail.com>

---
+++ compraApp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name compraAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the compraAppApp
+ */
+angular.module('compraApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ compraApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name compraAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the compraAppApp
+ */
+angular.module('compraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
