820967fe151555c87ae64f1f0710c6b1a6f7bc53
Felipe Moraes <felipealvesdemoraes@gmail.com>
2016-04-04 00:52:27 -0300

    Versao 1 do backend

---
+++ backend/src/main/java/api/RestConfig.java
@@ -0,0 +1,19 @@
+package api;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("v1")
+public class RestConfig extends ResourceConfig {
+
+	public RestConfig()
+	{
+		// register with Jersey (assumes all Jersey resource classes are in this package or below)
+		packages(this.getClass().getPackage().getName());
+
+		// activate Jackson-based JSON support
+		register(JacksonFeature.class);
+	}
+}
---
+++ backend/src/main/java/api/resources/QueroVenderResource.java
@@ -0,0 +1,18 @@
+package api.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("queroVender")
+public class QueroVenderResource {
+
+	@GET
+	@Path("{modelo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String pesquisa(@PathParam(value = "modelo") String modelo) {
+        return "Você está querendo vender um: " + modelo;
+    }
+}
