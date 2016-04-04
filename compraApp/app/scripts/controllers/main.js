ccfa0484a933d3e95f669ffd74b586dbe80350d2
Felipe Alves de Moraes <felipe.moraes@spcbr.int.br>
2016-04-04 10:51:57 -0300

    Comunicação Front-end, Backend

--- compraApp/app/scripts/controllers/main.js
+++ compraApp/app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the compraAppApp
  */
 angular.module('compraApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  	var url = 'http://localhost:8080/v1/queroVender/';
+  	$scope.query = '';
+  	$scope.resposta = '';
+
+    $scope.pesquisar = function(query) {$http.get(url + query).then(function(response){
+    			$scope.resposta = response.data;
+          console.log(response);
+		    }, function(response) {
+            console.log(response);
+		    });
+		};
+  }]);
